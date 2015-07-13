430df134f96331e3993461ffe3d1d2bfeb74a656
Ray Lv <ray.lv@sap.com>
2015-07-13 14:21:10 +0800

    20150713

---
+++ MyExpressExercise02/routes/index.js
@@ -0,0 +1,32 @@
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res) {
+	res.render('index', {
+		title : 'Express'
+	});
+};
+
+exports.userlist = function(db) {
+	return function(req, res) {
+		var collection = db.get('users');
+		collection.find({}, {}, function(e, docs) {
+			res.render('userlist', {
+				"userlist" : docs
+			});
+		});
+	};
+};
+
+exports.login = function(req, res) {
+	res.render('login', {
+		title : 'Smart Gallery'
+	});
+};
+
+exports.login2 = function(req, res) {
+	res.render('login2', {
+		title : 'Smart Gallery'
+	});
+};
